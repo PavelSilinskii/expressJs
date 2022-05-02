6ae69d06b2d4d437ca9474479459ad4b34f2cfb4
Pavel <work322@gmail.com>
2022-05-03 01:56:42 +0300

    create router sheme

---
+++ routes/home.js
@@ -0,0 +1,13 @@
+//const express.Router = require('express')
+const {Router} = require('express')
+const router = Router()
+
+router.get('/', (req,res)=>{
+    res.render('index',{
+        title:'Главная страница',
+        isHome: true
+    })
+})
+
+
+module.exports = router
