f63dc1498f2581c4664390abe7e843a801cf481d
Pavel <work322@gmail.com>
2022-05-13 00:52:46 +0300

    create detail page at course

--- models/course.js
+++ models/course.js
@@ -62,6 +62,14 @@ class Course{
         })
       
     }
+    static async getById(id){
+        const courses = await Course.getAll()
+        return courses.find(c => c.id ===  id) 
+    }
+
+
+
+
 }
 
 module.exports = Course
