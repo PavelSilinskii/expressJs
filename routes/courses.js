35ade9fffe9bf52de2ca9ea454d49ed5aff6e89f
Pavel <work322@gmail.com>
2022-05-19 23:54:01 +0300

    continue

--- routes/courses.js
+++ routes/courses.js
@@ -15,7 +15,7 @@ router.get('/:id',async(req,res)=>{
     const course = await Course.getById(req.params.id)
     res.render('course', {
         layout: 'empty',
-        title: `Курс {{course.title}}`,
+        title: `Курс ${course.title}`,
         course
     })
 })
